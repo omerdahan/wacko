be3760bf71403f0f821732c22d28e15404a4d073
omerdahan <42843159+omerdahan@users.noreply.github.com>
2018-09-06 16:55:10 +0300

    ex2 answer

---
+++ 2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  2.py
+#  
+#  Copyright 2018 Omer Dan <omerd@localhost.localdomain>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+print("enter your 1st string!")
+str1=input()
+print("enter your 2nd string")
+str2 = input()
+str1 == str2
+print("enter your num between 1 and 3")
+var1 = input()
+if int(var1) > 3:
+	print("number must be between 1 and 3 TERMINATED!")
+	sys.exit()
+	
+	if str1 > str2:
+		str1 == str2
+		print(str1 , "is larger")
+		StrCount = len(str1)
+		FirstChar = str2[0:1]
+		new_str =
+		str1.replace(FirstChar ,"omer")
+		MergedStr = str1+str2+str(StrCount)
+		my_count = MergedStr.coubt('A')
+		
+		else:
+			print(str2, "is larger")
+			str1 == str2
+			StrCount = len(str2)
+			FirstChar = str1[0,1]
+			new_str =
+			str1.replace(FirstChar ,"omer")
+			MergedStr = str1+str2+str(StrCount)
+			my_count = MergedStr.count('A')
